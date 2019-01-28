b8abb5f6c24f7eb5eeabedeb26855eab958d0121
Ashton Armstrong <46547842+TehCosmicCeres@users.noreply.github.com>
2019-01-27 23:40:08 -0600

    Create personal.php

---
+++ personal.php
@@ -0,0 +1,20 @@
+<?php
+echo '<!DOCTYPE html>';
+echo '<!--';
+echo '--------------';
+echo 'Programmer:  Ashton Armstrong';
+echo 'Course:      ITSE-1306 (Intro to PHP)';
+echo 'Instructor:  Cesar "Coach" Marrero';
+echo 'Assignment:  Write the traditional "Hello World" program';
+echo 'Description: Its a traditional Hello World program with personal stuff thrown in there.';
+echo '--------------';
+echo '-->';
+echo '<html>';
+echo '<head>';
+echo '<title>Student Personal Page</title>';
+echo '</head>';
+echo '<body>';
+echo '<p>Hello World, my name is Ashton Armstrong.</p>';
+echo '</body>';
+echo '</html>';
+?>
