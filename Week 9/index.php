60fd1ab6118a132218cb63cd7a2c56a2aa5f14b9
Ashton Armstrong <46547842+TehCosmicCeres@users.noreply.github.com>
2019-03-25 22:01:32 -0500

    Add files via upload

---
+++ Week 9/index.php
@@ -0,0 +1,17 @@
+<?php
+// --------------
+// -- Programmer:  Ashton Armstrong
+// -- Course:      ITSE-1306 (Intro to PHP)
+// -- Instructor:  Cesar "Coach" Marrero
+// -- Assignment:  Shoes R Us
+// -- Description: A dynamic Website about a bad shoe store
+// ---------------
+require('header.html');
+?>
+
+<h2>Welcome to Shoes R Us, Used shoes, at a decent price!</h2>
+        <p>Click the links above to view the shoes, or 
+            click the home link to return to the home page.</p>
+<?php
+require('footer.html');
+?>
---
+++ Week 9/shoe3.php
@@ -0,0 +1,18 @@
+<?php
+// --------------
+// -- Programmer:  Ashton Armstrong
+// -- Course:      ITSE-1306 (Intro to PHP)
+// -- Instructor:  Cesar "Coach" Marrero
+// -- Assignment:  Shoes R Us
+// -- Description: A dynamic Website about a bad shoe store
+// ---------------
+require('header.html');
+?>
+
+<h2>Welcome to Shoes R Us, Used shoes, at a decent price!</h2>
+<img src="shoe3.jpeg" alt="A shoe." title="A shoe." width="600" height="450"/>
+        <p>Click the links above to view the shoes, or 
+            click the home link to return to the home page.</p>
+<?php
+require('footer.html');
+?>
