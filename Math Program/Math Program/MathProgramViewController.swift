3e739396661aca19d29c5197810242a9f20440b5
Ashton Armstrong <46547842+TehCosmicCeres@users.noreply.github.com>
2019-05-29 23:02:14 -0500

    Add files via upload

---
+++ Math Program/Math Program/MathProgramViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MathProgramViewController.swift
+//  Math Program
+//
+//  Created by Ashton Armstrong on 3/8/1398 AP.
+//  Copyright © 1398 Ashton Armstrong. All rights reserved.
+//
+
+import UIKit
+class MathProgramViewController: UIViewController {
+    let tub = 0
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
---
+++ Math Program/Math Program/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Math Program
+//
+//  Created by Ashton Armstrong on 3/8/1398 AP.
+//  Copyright © 1398 Ashton Armstrong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
