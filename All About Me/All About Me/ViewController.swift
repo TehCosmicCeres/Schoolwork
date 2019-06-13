214c74bd55e069421eef4305a38e8ab90d433270
Ashton Armstrong <46547842+TehCosmicCeres@users.noreply.github.com>
2019-06-13 06:52:47 -0700

    Add files via upload

---
+++ All About Me/All About Me/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  All About Me
+//
+//  Created by Ashton Armstrong on 3/23/1398 AP.
+//  Copyright © 1398 Ashton Armstrong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
